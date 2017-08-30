7756c28e2e0df89d29284cd696d119ea0b65ce2e
VandeurenGlenn <vandeurenglenn@gmail.com>
2017-08-30 23:05:16 +0200

    :trollface: Version: 0.9.0-preview

---
+++ custom-app-layout.js
@@ -0,0 +1,115 @@
+import LitMixin from '/node_modules/backed/mixins/lit-mixin.js';
+import PropertyMixin from '/node_modules/backed/mixins/property-mixin.js';
+
+/**
+ * @example
+ * <custom-app-layout>
+ *   <header slot="header" fixed></header>
+ *   <section slot="content"></section> // appears under the header
+ * </custom-app-layout>
+ * @extends LitMixin, PropertyMixin, HTMLElement
+ */
+class CustomAppLayout extends LitMixin(PropertyMixin(HTMLElement)) {
+  /**
+   * @param {object} options contains properties, observers, listeners, etc...
+   */
+  constructor(options = {properties: {}}) {
+    const properties = {
+      firstRender: {value: true, renderer: 'render'}
+    }
+    Object.assign(options.properties, properties); // merge properties
+
+    super(options);
+  }
+  // iterate trough slots untill no slot is found
+  slotted(slot) {
+    slot = slot.assignedNodes()
+    if (slot[0].localName === 'slot') {
+      return this.slotted(slot[0]);
+    } else {
+      for (const node of slot) {
+        if (node.nodeType === 1) {
+          return node
+        }
+      }
+    }
+    return slot;
+  }
+
+  get content() {
+    return this.slotted(this.shadowRoot.querySelector('slot[name="content"]'));
+  }
+
+  get header() {
+    return this.slotted(this.shadowRoot.querySelector('slot[name="header"]'));
+  }
+
+  get container() {
+    return this.shadowRoot.querySelector('.content-container');
+  }
+
+  render() {
+    if (this.firstRender === false) { // do nothing on firstRender
+      const header = this.header;
+      const containerStyle = this.container.style;
+      const headerHeight = header.offsetHeight;
+
+      if (header.hasAttribute('fixed') && !header.hasAttribute('condenses')) {
+        // If the header size does not change and we're using a scrolling region, exclude
+        // the header area from the scrolling region so that the header doesn't overlap
+        // the scrollbar.
+        containerStyle.marginTop = headerHeight + 'px';
+        containerStyle.paddingTop = '';
+      } else {
+        containerStyle.paddingTop = headerHeight + 'px';
+        containerStyle.marginTop = '';
+      }
+    } else {
+      this.firstRender = false;
+    }
+    return html`
+      <style>
+        :host {
+          display: block;
+          position: relative;
+          height: 100%;
+          z-index: 0;
+          display: flex;
+          flex-direction: column;
+        }
+        :host([fullbleed]) {
+          position: absolute;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+        }
+        ::slotted([slot="header"]) {
+          position: absolute;
+          top: 0;
+          left: 0;
+          right: 0;
+          z-index: 1;
+        }
+        .content-container {
+          position: absolute;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          flex: 1;
+          flex-direction: column;
+          z-index: 0;
+          overflow-y: auto;
+          -webkit-overflow-scrolling: touch;
+        }
+      </style>
+      <slot name="header"></slot>
+      <span class="content-container">
+        <slot name="content"></slot>
+      </span>
+    `;
+  }
+};
+
+export default customElements.define('custom-app-layout', CustomAppLayout);
